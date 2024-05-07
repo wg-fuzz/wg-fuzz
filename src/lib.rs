086303e9c2bfe14a61176091fdb59449ee82f213
Daniel <in.lieu.of.leu@gmail.com>
2024-05-07 17:14:54 +0100

    Extract some JS code into txt files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod random_adapter;
 mod random_device;
 
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
@@ -18,6 +19,7 @@ struct WebGPUProgram {
 }
 
 impl WebGPUProgram {
+
   fn new(dawn_type: DawnType) -> Self {
     let dawn_import_arguments = match dawn_type {
       DawnType::Safe => String::from("'enable-dawn-features=allow_unsafe_apis,disable_symbol_renaming'"),
@@ -35,126 +37,17 @@ let navigator = {{ gpu: create([{dawn_import_arguments}]), }};");
       dawn_import,
       random_adapter,
       random_device,
-      program: String::from("\
-      // Define global buffer size
-      const BUFFER_SIZE = 1000;
-    
-      const shader = `
-      @group(0) @binding(0)
-      var<storage, read_write> output: array<f32>;
-    
-      @compute @workgroup_size(64)
-      fn main(
-        @builtin(global_invocation_id)
-        global_id : vec3u,
-    
-        @builtin(local_invocation_id)
-        local_id : vec3u,
-      ) {
-        // Avoid accessing the buffer out of bounds
-        if (global_id.x >= ${BUFFER_SIZE}) {
-          return;
-        }
-    
-        output[global_id.x] =
-          f32(global_id.x) * 1000. + f32(local_id.x);
-      }
-      `;
-    
-      const shaderModule = device.createShaderModule(
-        {
-          code: shader
-        }
-      );
-    
-      const output = device.createBuffer({
-        size: BUFFER_SIZE,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
-      });
-    
-      const stagingBuffer = device.createBuffer({
-        size: BUFFER_SIZE,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
-      });
-    
-      const bindGroupLayout = device.createBindGroupLayout({
-        entries: [
-          {
-            binding: 0,
-            visibility: GPUShaderStage.COMPUTE,
-            buffer: {
-              type: \"storage\",
-            },
-          },
-        ],
-      });
-    
-      const bindGroup = device.createBindGroup({
-        layout: bindGroupLayout,
-        entries: [
-          {
-            binding: 0,
-            resource: {
-              buffer: output,
-            },
-          },
-        ],
-      });
-    
-      const computePipeline = device.createComputePipeline({
-        layout: device.createPipelineLayout({
-          bindGroupLayouts: [bindGroupLayout],
-        }),
-        compute: {
-          module: shaderModule,
-          entryPoint: \"main\",
-        },
-      });
-    
-      const commandEncoder = device.createCommandEncoder();
-      const passEncoder = commandEncoder.beginComputePass();
-    
-      passEncoder.setPipeline(computePipeline);
-      passEncoder.setBindGroup(0, bindGroup);
-      passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64));
-    
-      passEncoder.end();
-    
-      // Copy output buffer to staging buffer
-      commandEncoder.copyBufferToBuffer(
-        output,
-        0, // Source offset
-        stagingBuffer,
-        0, // Destination offset
-        BUFFER_SIZE,
-      );
-    
-      // End frame by passing array of command buffers to command queue for execution
-      device.queue.submit([commandEncoder.finish()]);
-    
-      // map staging buffer to read results back to JS
-      await stagingBuffer.mapAsync(
-        GPUMapMode.READ,
-        0, // Offset
-        BUFFER_SIZE, // Length
-      );
-    
-      const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
-      const data = copyArrayBuffer.slice();
-      stagingBuffer.unmap();
-      console.log(new Float32Array(data));")
+      program: fs::read_to_string("code_samples/webgpu_program.txt").unwrap(),
     }
   }
+  
 
   fn to_string(&self) -> String {
     let mut program_content = String::new();
     program_content.push_str(&self.dawn_import);
     program_content.push_str("\n\n");
     program_content.push_str("async function init() {\n");
-    program_content.push_str("\
-if (!navigator.gpu) {
-  throw Error(\"WebGPU not supported.\");
-}\n\n");
+    program_content.push_str(&fs::read_to_string("code_samples/navigator_check.txt").unwrap());
     program_content.push_str(&self.random_adapter);
     program_content.push_str(&self.random_device);
     program_content.push_str(&self.program);
@@ -162,6 +55,7 @@ if (!navigator.gpu) {
 
     program_content
   }
+
 }
 
 pub fn fuzz() {
