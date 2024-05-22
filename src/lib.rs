b23c79aece4d591ee79ac7a770970e554c0f2c8c
Daniel <in.lieu.of.leu@gmail.com>
2024-05-22 19:47:53 +0100

    Remove unneeded code

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
 
-const INDENT: &str = "    ";
-
-enum DawnType {
-  Safe,
-  AllowUnsafeAPIs,
-}
-
 enum APICall {
   CreateAdapter,
   CreateDevice,
@@ -24,53 +17,6 @@ enum APICall {
   SubmitWork
 }
 
-struct WebGPUProgram {
-  dawn_import: String,
-  random_adapter: String,
-  random_device: String,
-  program: String,
-}
-
-impl WebGPUProgram {
-
-  fn new(dawn_type: DawnType) -> Self {
-    let dawn_import_arguments = match dawn_type {
-      DawnType::Safe => String::from("'enable-dawn-features=allow_unsafe_apis,disable_symbol_renaming'"),
-      DawnType::AllowUnsafeAPIs => String::from("")
-    };
-    let dawn_import = format!("\
-const {{ create, globals }} = require('./dawn.node');
-Object.assign(globalThis, globals); // Provides constants like GPUBufferUsage.MAP_READ
-let navigator = {{ gpu: create([{dawn_import_arguments}]), }};");
-
-    let random_adapter = random_adapter::get_random_adapter();
-    let random_device = random_device::get_random_device();
-
-    Self {
-      dawn_import,
-      random_adapter,
-      random_device,
-      program: fs::read_to_string("code_samples/webgpu_program.txt").unwrap(),
-    }
-  }
-  
-
-  fn to_string(&self) -> String {
-    let mut program_content = String::new();
-    program_content.push_str(&self.dawn_import);
-    program_content.push_str("\n\n");
-    program_content.push_str("async function init() {\n");
-    program_content.push_str(&fs::read_to_string("code_samples/navigator_check.txt").unwrap());
-    // program_content.push_str(&self.random_adapter);
-    // program_content.push_str(&self.random_device);
-    // program_content.push_str(&self.program);
-    program_content.push_str("}\n\ninit();");
-
-    program_content
-  }
-
-}
-
 pub fn fuzz() {
     loop {
         fuzz_once().unwrap();
@@ -80,14 +26,12 @@ pub fn fuzz() {
 pub fn fuzz_once() -> std::io::Result<()> {
     let mut file = File::create("test.js")?;
 
-    // let sample_program = WebGPUProgram::new(DawnType::AllowUnsafeAPIs).to_string();
-
     let mut sample_program = String::from("");
 
     sample_program.push_str("\
 const {{ create, globals }} = require('./dawn.node');
 Object.assign(globalThis, globals); // Provides constants like GPUBufferUsage.MAP_READ
-let navigator = {{ gpu: create([]), }};");
+let navigator = {{ gpu: create(['enable-dawn-features=allow_unsafe_apis,disable_symbol_renaming']), }};");
     sample_program.push_str("\n\n");
     sample_program.push_str("async function init() {\n");
     sample_program.push_str(&fs::read_to_string("code_samples/navigator_check.txt").unwrap());
