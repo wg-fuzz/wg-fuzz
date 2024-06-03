8f9c38df4dc94c5dd2299da6c1b5919962c2a192
Daniel <in.lieu.of.leu@gmail.com>
2024-06-03 16:09:23 +0100

    Remove unused API call comments

--- crates/act/src/lib.rs
+++ crates/act/src/lib.rs
@@ -40,17 +40,8 @@ pub enum APICall {
     CreateDevice(GPUAdapter),
     // CreateBuffer(GPUDevice),
     // CreateTexture(GPUDevice),
-    // CreateExternalTexture(GPUDevice, HTMLVideo),
-    // CreateHTMLVideo(),
-    // CreateSampler(GPUDevice),
-    // CreateQuerySet(GPUDevice),
     CreateShaderModule(GPUDevice),
-    // CreateBindGroup(GPUDevice),
-    // CreateBindGroupLayout(GPUDevice),
-    // CreatePipelineLayout(GPUDevice),
     CreateComputePipeline(GPUDevice, GPUShaderModule),
-    // CreateRenderPipeline(GPUDevice, GPUShaderModule),
-    // CreateRenderBundleEncoder(GPUDevice),
     CreateCommandEncoder(GPUDevice),
     CreateComputePass(GPUCommandEncoder),
     SetComputePassPipeline(GPUComputePassEncoder, GPUComputePipeline),
@@ -92,34 +83,6 @@ impl APICall {
             //         panic!("created_resource for CreateTexture() call is not a texture!")
             //     }
             // },
-            // CreateExternalTexture(device, html_video) => {
-            //     if let Resource::GPUExternalTexture(external_texture) = created_resource {
-            //         return format!("const {} = {}.importExternalTexture({{  }});", adapter.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateExternalTexture() call is not an external texture!")
-            //     }
-            // },
-            // CreateHTMLVideo() => {
-            //     if let Resource::HTMLVideo(html_video) = created_resource {
-            //         return format!("const {} = await navigator.gpu.requestAdapter();", adapter.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateHTMLVideo() call is not a HTML video!")
-            //     }
-            // },
-            // CreateSampler(device) => {
-            //     if let Resource::GPUSampler(sampler) = created_resource {
-            //         return format!("const {} = {}.createSampler();", sampler.var_name, device.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateSampler() call is not a sampler!")
-            //     }
-            // },
-            // CreateQuerySet(device) => {
-            //     if let Resource::GPUQuerySet(query_set) = created_resource {
-            //         return format!("const {} = {}.createQuerySet({{ count: 100, type: \"occlusion\" }});", query_set.var_name, device.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateQuerySet() call is not a query set!")
-            //     }
-            // },
             CreateShaderModule(device) => {
                 if let Resource::GPUShaderModule(shader_module) = created_resource {
                     let file_name = format!("out/{}.wgsl", shader_module.var_name);
@@ -157,27 +120,6 @@ impl APICall {
                     panic!("created_resource for CreateShaderModule() call is not a shader module!")
                 }
             },
-            // CreateBindGroup(device) => {
-            //     if let Resource::GPUBindGroup(bind_group) = created_resource {
-            //         return format!("const {} = {}.createBindGroup({{ entries: [], layout: \"auto\" }});", bind_group.var_name, device.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateBindGroup() call is not an bind group!")
-            //     }
-            // },
-            // CreateBindGroupLayout(device) => {
-            //     if let Resource::GPUBindGroupLayout(bind_group_layout) = created_resource {
-            //         return format!("const {} = {}.createBindGroupLayout({{ entries: [] }});", bind_group_layout.var_name, device.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateBindGroupLayout() call is not a bind group layout!")
-            //     }
-            // },
-            // CreatePipelineLayout(device) => {
-            //     if let Resource::GPUPipelineLayout(pipeline_layout) = created_resource {
-            //         return format!("const {} = {}.createPipelineLayout({{ bindGroupLayouts: [] }});", pipeline_layout.var_name, device.var_name);
-            //     } else {
-            //         panic!("created_resource for CreatePipelineLayout() call is not a pipeline layout!")
-            //     }
-            // },
             CreateComputePipeline(device, shader_module) => {
                 if let Resource::GPUComputePipeline(compute_pipeline) = created_resource {
                     return format!("const {} = {}.createComputePipeline({{ layout: \"auto\", compute: {{ module: {}, entryPoint: \"main\" }} }});", compute_pipeline.var_name, device.var_name, shader_module.var_name);
@@ -185,20 +127,6 @@ impl APICall {
                     panic!("created_resource for CreateComputePipeline() call is not a compute pipeline!")
                 }
             },
-            // CreateRenderPipeline(device, shader_module) => {
-            //     if let Resource::GPURenderPipeline(render_pipeline) = created_resource {
-            //         return format!("const {} = await navigator.gpu.requestAdapter();", adapter.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateRenderPipeline() call is not a compute pipeline!")
-            //     }
-            // },
-            // CreateRenderBundleEncoder(device) => {
-            //     if let Resource::GPURenderBundleEncoder(render_bundle_encoder) = created_resource {
-            //         return format!("const {} = {}.createRenderBundleEncoder({{ colorFormats: [] }});", render_bundle_encoder.var_name, device.var_name);
-            //     } else {
-            //         panic!("created_resource for CreateRenderBundleEncoder() call is not a render bundle encoder!")
-            //     }
-            // },
             CreateCommandEncoder(device) => {
                 if let Resource::GPUCommandEncoder(command_encoder) = created_resource {
                     return format!("const {} = {}.createCommandEncoder({{ label: \"{}\" }});", command_encoder.var_name, device.var_name, command_encoder.var_name);
