bc50ec810014526d6b422ee4e958952f4a1bdd0e
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:15:18 +0100

    Refactor GPUShaderModule into primitives/shader_module.rs

--- crates/art/src/lib.rs
+++ crates/art/src/lib.rs
@@ -167,30 +167,6 @@ impl GPUQuerySet {
 }
 
 #[derive(Debug, Clone)]
-pub struct GPUShaderModule {
-    pub num: usize,
-    pub var_name: String,
-
-    pub compute_or_render: String,
-}
-
-impl GPUShaderModule {
-    pub fn new(device: &GPUDevice, compute_or_render: String) -> GPUShaderModule {
-        let num_adapter = device.num_adapter;
-        let num_device = device.num;
-        let num = device.shader_modules.len();
-        let name = format!("shader_module{}{}{}", num_adapter, num_device, num);
-
-        GPUShaderModule {
-            num,
-            var_name: name,
-
-            compute_or_render,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
 pub struct GPUBindGroup {
     pub num: usize,
     pub var_name: String
--- crates/art/src/primitives/mod.rs
+++ crates/art/src/primitives/mod.rs
@@ -3,9 +3,11 @@ pub mod texture;
 pub mod texture_view;
 pub mod external_texture;
 pub mod sampler;
+pub mod shader_module;
 
 pub use buffer::*;
 pub use texture::*;
 pub use texture_view::*;
 pub use external_texture::*;
 pub use sampler::*;
+pub use shader_module::*;
