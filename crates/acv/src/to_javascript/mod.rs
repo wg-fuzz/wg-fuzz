e14a0a10a1cf88fe4d2b258fc1cc87a47962ab03
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 18:28:27 +0100

    Revert previous refactoring style

--- crates/acv/src/to_javascript/mod.rs
+++ crates/acv/src/to_javascript/mod.rs
@@ -1,28 +1,106 @@
-pub mod program_setup;
-pub mod primitives;
-pub mod encoders;
-pub mod pipeline_setup;
-
-pub use program_setup::*;
-pub use primitives::*;
-pub use encoders::*;
-pub use pipeline_setup::*;
-
 use crate::*;
 
 impl APICall {
     pub fn to_javascript(&self, created_resource: &Resource) -> String {
         match self {
-            PrintWGSLLanguageFeatures() => print_wgsl_language_features(),
-            PrintPreferredCanvasFormat() => print_preferred_canvas_format(),
-            CreateArray() => create_array(created_resource),
+            PrintWGSLLanguageFeatures() => {
+                return String::from("console.log(navigator.gpu.wgslLanguageFeatures.size);
 
-            CreateAdapter() => create_adapter(created_resource),
-            PrintAdapterInfo(adapter) => print_adapter_info(adapter),
+    for (const value of navigator.gpu.wgslLanguageFeatures.keys()) {
+        console.log(value);
+    }");
+            },
+            PrintPreferredCanvasFormat() => {
+                return String::from("console.log(navigator.gpu.getPreferredCanvasFormat());");
+            },
+            CreateArray() => {
+                if let Resource::RandomArray(array) = created_resource {
+                    let mut random_floats = String::new();
+                    let float_choices = ["-1.0", "-0.75", "-0.5", "-0.25", "0.0", "0.25", "0.5", "0.75", "1.0"];
+                    let mut rand = rand::thread_rng();
+                    for _ in 0..100 {
+                        let i = rand.gen_range(0..float_choices.len());
+                        random_floats.push_str(float_choices[i]);
+                        random_floats.push_str(", ");
+                    }
+                    return format!("const {} = new Float32Array([{}]);", array.var_name, random_floats);
+                } else {
+                    panic!("created_resource for CreateDevice() call is not a device!")
+                }
+            },
 
-            CreateDevice(adapter) => create_device(created_resource, adapter),
-            PrintDeviceInfo(device) => print_device_info(device),
-            DestroyDevice(device) => destroy_device(device),
+            CreateAdapter() => {
+                if let Resource::GPUAdapter(adapter) = created_resource {
+                    let mut rng = rand::thread_rng();
+                    let i = rng.gen_range(0..3);
+                    let random_power_preference = match i {
+                        0 => "undefined",
+                        1 => "\"low-power\"",
+                        2 => "\"high-performance\"",
+                        _ => "undefined",
+                    };
+                    return format!("const {} = await navigator.gpu.requestAdapter({{
+                    powerPreference: {},
+                    label: \"{}\"
+                }});", adapter.var_name, random_power_preference, adapter.var_name);
+                } else {
+                    panic!("created_resource for CreateAdapter() call is not an adapter!")
+                }
+            },
+            PrintAdapterInfo(adapter) => {
+                return format!("console.log({}.features.size);
+
+    for (const value of {}.features.keys()) {{
+        console.log(value);
+    }}
+    
+    console.log({}.isFallbackAdapter);
+    
+    console.log({}.limits.size);
+    
+    for (const [key, value] of Object.entries({}.limits)) {{
+        console.log(key);
+        console.log(value);
+    }}
+
+    {{
+        const adapterInfo = await {}.requestAdapterInfo();
+        console.log(adapterInfo.vendor);
+        console.log(adapterInfo.architecture);
+        console.log(adapterInfo.device);
+        console.log(adapterInfo.description);
+    }}", adapter.var_name, adapter.var_name, adapter.var_name, adapter.var_name, adapter.var_name, adapter.var_name);
+            },
+
+            CreateDevice(adapter) => {
+                if let Resource::GPUDevice(device) = created_resource {
+                    return format!("const {} = await {}.requestDevice({{ label: \"{}\" }});", device.var_name, adapter.var_name, device.var_name);
+                } else {
+                    panic!("created_resource for CreateDevice() call is not a device!")
+                }
+            },
+            PrintDeviceInfo(device) => {
+                return format!("console.log({}.features.size);
+    
+    for (const value of {}.features.keys()) {{
+        console.log(value);
+    }}
+    
+    console.log({}.limits.size);
+    
+    for (const [key, value] of Object.entries({}.limits)) {{
+        console.log(key);
+        console.log(value);
+    }}
+
+    {}.lost.then((info) => {{
+        console.error(`WebGPU device was lost: ${{info.message}}`);
+        console.log(info.reason);
+    }});", device.var_name, device.var_name, device.var_name, device.var_name, device.var_name);
+            },
+            DestroyDevice(device) => {
+                return format!("{}.destroy();", device.var_name);
+            },
 
             CreateRandomBuffer(device) => {
                 if let Resource::GPUBuffer(buffer) = created_resource {
@@ -528,12 +606,40 @@ impl APICall {
                     panic!("created_resource for CreateCommandBuffer() call is not a command buffer!")
                 }
             }
-            SubmitQueueRandom(device, command_encoders) => submit_queue_random(device, command_encoders),
-            WaitSubmittedWork(device) => wait_submitted_work(device),
+            SubmitQueueRandom(device, command_encoders) => {
+                let mut command_buffers_str = String::from("[");
+                for command_encoder in command_encoders {
+                    command_buffers_str.push_str(&command_encoder.command_buffer.as_ref().unwrap().var_name);
+                    command_buffers_str.push_str(", ");
+                }
+                command_buffers_str.push_str("]");
+                return format!("{}.queue.submit({});", device.var_name, command_buffers_str);
+            },
+            WaitSubmittedWork(device) => {
+                return format!("{}.queue.onSubmittedWorkDone();", device.var_name);
+            },
 
-            PushRandomErrorScope(device) => push_random_error_scope(device),
-            PopErrorScope(device) => pop_error_scope(device),
-            // AddUncapturedErrorListener(device) => add_uncaptured_error_listener(device),
+            PushRandomErrorScope(device) => {
+                let mut rng = rand::thread_rng();
+                let i = rng.gen_range(0..3);
+                let random_error_type = match i {
+                    0 => "\"internal\"",
+                    1 => "\"out-of-memory\"",
+                    2 => "\"validation\"",
+                    _ => "\"internal\"",
+                };
+                return format!("{}.pushErrorScope({});", device.var_name, random_error_type);
+            },
+            PopErrorScope(device) => {
+                return format!("{}.popErrorScope().then((error) => {{
+        if (error) {{
+            console.error(`An error occurred: ${{error.message}}`);
+        }}
+    }});", device.var_name);
+            },
+            // AddUncapturedErrorListener(device) => {
+            //     return format!("console.log(typeof {}.onuncapturederror);", device.var_name);
+            // },
         }
     }
 }
--- crates/acv/src/to_javascript/primitives.rs
+++
@@ -1,5 +0,0 @@
-use crate::*;
-
-// pub fn wait_submitted_work(device: &GPUDevice) -> String {
-//     return format!("{}.queue.onSubmittedWorkDone();", device.var_name);
-// }
