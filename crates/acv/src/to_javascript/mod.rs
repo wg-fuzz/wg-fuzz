22bb4033844e281a734af152cbe763e61fd8adf7
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 18:37:13 +0100

    Refactor to_javascript program.rs

--- crates/acv/src/to_javascript/mod.rs
+++ crates/acv/src/to_javascript/mod.rs
@@ -1,33 +1,12 @@
 use crate::*;
 
+mod program;
+use program::*;
+
 impl APICall {
     pub fn to_javascript(&self, created_resource: &Resource) -> String {
         match self {
-            PrintWGSLLanguageFeatures() => {
-                return String::from("console.log(navigator.gpu.wgslLanguageFeatures.size);
-
-    for (const value of navigator.gpu.wgslLanguageFeatures.keys()) {
-        console.log(value);
-    }");
-            },
-            PrintPreferredCanvasFormat() => {
-                return String::from("console.log(navigator.gpu.getPreferredCanvasFormat());");
-            },
-            CreateArray() => {
-                if let Resource::RandomArray(array) = created_resource {
-                    let mut random_floats = String::new();
-                    let float_choices = ["-1.0", "-0.75", "-0.5", "-0.25", "0.0", "0.25", "0.5", "0.75", "1.0"];
-                    let mut rand = rand::thread_rng();
-                    for _ in 0..100 {
-                        let i = rand.gen_range(0..float_choices.len());
-                        random_floats.push_str(float_choices[i]);
-                        random_floats.push_str(", ");
-                    }
-                    return format!("const {} = new Float32Array([{}]);", array.var_name, random_floats);
-                } else {
-                    panic!("created_resource for CreateDevice() call is not a device!")
-                }
-            },
+            PrintWGSLLanguageFeatures() | PrintPreferredCanvasFormat() | CreateArray() => program_to_js(self, created_resource),
 
             CreateAdapter() => {
                 if let Resource::GPUAdapter(adapter) = created_resource {
