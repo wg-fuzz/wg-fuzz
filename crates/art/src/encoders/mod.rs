f69fbe607ac1d8b2a5d1030486feb43258b882dd
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:26:10 +0100

    Refactor GPUCommandBuffer into encoders/command_buffer.rs

--- crates/art/src/encoders/mod.rs
+++ crates/art/src/encoders/mod.rs
@@ -1,7 +1,9 @@
 pub mod command_encoder;
 pub mod compute_pass_encoder;
 pub mod render_pass_encoder;
+pub mod command_buffer;
 
 pub use command_encoder::*;
 pub use compute_pass_encoder::*;
 pub use render_pass_encoder::*;
+pub use command_buffer::*;
--- crates/art/src/lib.rs
+++ crates/art/src/lib.rs
@@ -279,23 +279,3 @@ impl GPURenderBundleEncoder {
         }
     }
 }
-
-#[derive(Debug, Clone)]
-pub struct GPUCommandBuffer {
-    pub num: usize,
-    pub var_name: String
-}
-
-impl GPUCommandBuffer {
-    pub fn new(encoder: &GPUCommandEncoder) -> GPUCommandBuffer {
-        let num_adapter = encoder.num_adapter;
-        let num_device = encoder.num_device;
-        let num_encoder = encoder.num;
-        let name = format!("command_buffer{}{}{}", num_adapter, num_device, num_encoder);
-
-        GPUCommandBuffer {
-            num: num_encoder,
-            var_name: name
-        }
-    }
-}
