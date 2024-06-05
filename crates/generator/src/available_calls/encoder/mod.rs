863625fd6e8576673c6dee96cfa8b939f1905b2d
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 16:35:29 +0100

    Move encoder/master.rs into encoder/mod.rs

--- crates/generator/src/available_calls/encoder/mod.rs
+++ crates/generator/src/available_calls/encoder/mod.rs
@@ -1,5 +1,30 @@
 pub mod states;
-pub mod master;
 pub mod compute_pass;
 
-pub use master::*;
+use crate::*;
+use states::*;
+use compute_pass::*;
+
+pub fn add_manipulate_command_encoders(available_api_calls: &mut Vec<APICall>, device: &GPUDevice) -> bool {
+    let mut queue_command_encoders: Vec<GPUCommandEncoder> = Vec::new();
+
+    let mut all_command_encoders_submitted = true;
+
+    for command_encoder in &device.command_encoders {
+        let mut all_passes_finished = true;
+
+        all_passes_finished = add_manipulate_current_compute_pass(available_api_calls, device, command_encoder, all_passes_finished);
+
+        add_command_encoder_not_yet_buffer(available_api_calls, device, command_encoder, all_passes_finished);
+
+        add_command_encoder_is_buffer(command_encoder, &mut queue_command_encoders);
+        
+        all_command_encoders_submitted = add_command_encoder_is_submitted(command_encoder, all_command_encoders_submitted);
+    }
+
+    if queue_command_encoders.len() > 0 {
+        available_api_calls.extend([SubmitQueueRandom(device.clone(), queue_command_encoders)]);
+    }
+
+    all_command_encoders_submitted
+}
