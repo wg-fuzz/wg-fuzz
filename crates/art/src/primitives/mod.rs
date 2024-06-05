084bc0f76fd2c1756e1698f8b4cb46f5672ad0c3
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:19:03 +0100

    Refactor texture-like primitives under primitives/textures/

--- crates/art/src/primitives/mod.rs
+++ crates/art/src/primitives/mod.rs
@@ -1,15 +1,11 @@
 pub mod buffer;
-pub mod texture;
-pub mod texture_view;
-pub mod external_texture;
+pub mod textures;
 pub mod sampler;
 pub mod shader_module;
 pub mod query_set;
 
 pub use buffer::*;
-pub use texture::*;
-pub use texture_view::*;
-pub use external_texture::*;
+pub use textures::*;
 pub use sampler::*;
 pub use shader_module::*;
 pub use query_set::*;
---
+++ crates/art/src/primitives/textures/mod.rs
@@ -0,0 +1,7 @@
+pub mod texture;
+pub mod texture_view;
+pub mod external_texture;
+
+pub use texture::*;
+pub use texture_view::*;
+pub use external_texture::*;
