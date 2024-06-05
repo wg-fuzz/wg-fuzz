bb227887cc3c9b316d57ebb9315593ff98f61f35
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:07:33 +0100

    Refactor GPUTexture into primitives/texture.rs

--- crates/art/src/lib.rs
+++ crates/art/src/lib.rs
@@ -7,7 +7,7 @@
 
 pub mod enum_wrapper;
 pub mod primitives;
-pub use primitives::buffer::GPUBuffer;
+pub use primitives::*;
 
 #[derive(Debug, Clone)]
 pub struct ProgramResources {
@@ -124,46 +124,6 @@ impl RandomArray {
         RandomArray {
             num,
             var_name: name
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct GPUTexture {
-    pub num_adapter: usize,
-    pub num_device: usize,
-    pub num: usize,
-    pub var_name: String,
-
-    pub usage: String,
-    pub format: String,
-    // pub dimension: String,
-
-    pub texture_views: Vec<GPUTextureView>,
-
-    pub destroyed: bool,
-}
-
-impl GPUTexture {
-    pub fn new(device: &GPUDevice, usage: String, format: String) -> GPUTexture {
-        let num_adapter = device.num_adapter;
-        let num_device = device.num;
-        let num = device.textures.len();
-        let name = format!("texture{}{}{}", num_adapter, num_device, num);
-
-        GPUTexture {
-            num_adapter,
-            num_device,
-            num,
-            var_name: name,
-
-            usage,
-            format,
-            // dimension,
-
-            texture_views: Vec::new(),
-
-            destroyed: false
         }
     }
 }
--- crates/art/src/primitives/mod.rs
+++ crates/art/src/primitives/mod.rs
@@ -1,1 +1,5 @@
 pub mod buffer;
+pub mod texture;
+
+pub use buffer::*;
+pub use texture::*;
