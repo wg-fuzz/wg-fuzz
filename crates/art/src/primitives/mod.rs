bc6a65a51ddbaa31fb3288ee68dba179d9b3c4ea
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:08:43 +0100

    Refactor GPUTextureView into primitives/texture_view.rs

--- crates/art/src/primitives/mod.rs
+++ crates/art/src/primitives/mod.rs
@@ -1,5 +1,7 @@
 pub mod buffer;
 pub mod texture;
+pub mod texture_view;
 
 pub use buffer::*;
 pub use texture::*;
+pub use texture_view::*;
---
+++ crates/art/src/primitives/texture_view.rs
@@ -0,0 +1,22 @@
+use crate::*;
+
+#[derive(Debug, Clone)]
+pub struct GPUTextureView {
+    pub num: usize,
+    pub var_name: String
+}
+
+impl GPUTextureView {
+    pub fn new(texture: &GPUTexture) -> GPUTextureView {
+        let num_adapter = texture.num_adapter;
+        let num_device = texture.num_device;
+        let num_texture = texture.num;
+        let num = texture.texture_views.len();
+        let name = format!("texture_view{}{}{}{}", num_adapter, num_device, num_texture, num);
+
+        GPUTextureView {
+            num,
+            var_name: name
+        }
+    }
+}
