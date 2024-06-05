dd1dabc0ce26753bf765b78f19bea859806697ff
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 16:13:12 +0100

    Extract add_manipulate_current_compute_pass()

--- crates/generator/src/available_calls/mod.rs
+++ crates/generator/src/available_calls/mod.rs
@@ -98,35 +98,8 @@ fn add_manipulate_command_encoders(available_api_calls: &mut Vec<APICall>, devic
 
     for command_encoder in &device.command_encoders {
         let mut all_passes_finished = true;
-        for compute_pass in &command_encoder.compute_pass_encoders {
-            // This restricts a compute pass encoder to be set up in the following order in the final javascript program for simplicity
-            if let None = &compute_pass.pipeline {
-                for compute_pipeline in &device.compute_pipelines {
-                    available_api_calls.extend([SetComputePassPipeline(compute_pass.clone(), compute_pipeline.clone())])
-                }
-            } else if !compute_pass.bindgroup_set {
-                if let Some(compute_pipeline) = &compute_pass.pipeline {
-                    available_api_calls.extend([SetComputePassBindGroupTemplate(device.clone(), compute_pass.clone(), compute_pipeline.clone())]);
-                }
-            } else if !compute_pass.dispatched {
-                available_api_calls.extend([SetComputePassWorkgroups(compute_pass.clone()), SetComputePassWorkgroupsIndirect(device.clone(), compute_pass.clone())])
-            } else if !compute_pass.finished {
-                available_api_calls.extend([EndComputePass(compute_pass.clone())])
-            }
-
-            if !compute_pass.finished {
-                all_passes_finished = false;
-                available_api_calls.extend([InsertComputePassDebugMarker(compute_pass.clone())]);
-                if !compute_pass.debug_group_active {
-                    available_api_calls.extend([PushComputePassDebugGroup(compute_pass.clone())]);
-                }
-            }
-
-            if compute_pass.debug_group_active {
-                all_passes_finished = false;
-                available_api_calls.extend([PopComputePassDebugGroup(compute_pass.clone())])
-            }
-        }
+        
+        all_passes_finished = add_manipulate_current_compute_pass(available_api_calls, device, command_encoder, all_passes_finished);
 
         if let None = command_encoder.command_buffer {
             if all_passes_finished {
@@ -213,4 +186,38 @@ fn add_copy_texture(available_api_calls: &mut Vec<APICall>, device: &GPUDevice, 
             }
         }
     }
+}
+
+fn add_manipulate_current_compute_pass(available_api_calls: &mut Vec<APICall>, device: &GPUDevice, command_encoder: &GPUCommandEncoder, all_passes_finished: bool) -> bool {
+    let mut all_passes_finished = all_passes_finished;
+    for compute_pass in &command_encoder.compute_pass_encoders {
+        // This restricts a compute pass encoder to be set up in the following order in the final javascript program for simplicity
+        if let None = &compute_pass.pipeline {
+            for compute_pipeline in &device.compute_pipelines {
+                available_api_calls.extend([SetComputePassPipeline(compute_pass.clone(), compute_pipeline.clone())])
+            }
+        } else if !compute_pass.bindgroup_set {
+            if let Some(compute_pipeline) = &compute_pass.pipeline {
+                available_api_calls.extend([SetComputePassBindGroupTemplate(device.clone(), compute_pass.clone(), compute_pipeline.clone())]);
+            }
+        } else if !compute_pass.dispatched {
+            available_api_calls.extend([SetComputePassWorkgroups(compute_pass.clone()), SetComputePassWorkgroupsIndirect(device.clone(), compute_pass.clone())])
+        } else if !compute_pass.finished {
+            available_api_calls.extend([EndComputePass(compute_pass.clone())])
+        }
+
+        if !compute_pass.finished {
+            all_passes_finished = false;
+            available_api_calls.extend([InsertComputePassDebugMarker(compute_pass.clone())]);
+            if !compute_pass.debug_group_active {
+                available_api_calls.extend([PushComputePassDebugGroup(compute_pass.clone())]);
+            }
+        }
+
+        if compute_pass.debug_group_active {
+            all_passes_finished = false;
+            available_api_calls.extend([PopComputePassDebugGroup(compute_pass.clone())])
+        }
+    }
+    all_passes_finished
 }
