1f3ae9ce76ebb2074ed39081c7c03121e83b2fdc
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:33:49 +0100

    Refactor GPUBindGroupLayout into pipeline_setup/bind_group_layout.rs

--- crates/art/src/lib.rs
+++ crates/art/src/lib.rs
@@ -170,23 +170,3 @@ impl GPUBindGroup {
         }
     }
 }
-
-#[derive(Debug, Clone)]
-pub struct GPUBindGroupLayout {
-    pub num: usize,
-    pub var_name: String
-}
-
-impl GPUBindGroupLayout {
-    pub fn new(device: &GPUDevice) -> GPUBindGroupLayout {
-        let num_adapter = device.num_adapter;
-        let num_device = device.num;
-        let num = device.bind_group_layouts.len();
-        let name = format!("bind_group_layout{}{}{}", num_adapter, num_device, num);
-
-        GPUBindGroupLayout {
-            num,
-            var_name: name
-        }
-    }
-}
--- crates/art/src/pipeline_setup/mod.rs
+++ crates/art/src/pipeline_setup/mod.rs
@@ -1,7 +1,9 @@
 pub mod compute_pipeline;
 pub mod render_pipeline;
 pub mod pipeline_layout;
+pub mod bind_group_layout;
 
 pub use compute_pipeline::*;
 pub use render_pipeline::*;
 pub use pipeline_layout::*;
+pub use bind_group_layout::*;
