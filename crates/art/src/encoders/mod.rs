09ddd5826bd67efbbb5bcb81cf7f54fa29fdef4c
Daniel <in.lieu.of.leu@gmail.com>
2024-06-05 14:30:34 +0100

    Move RenderBundleEncoder.rs to encoders/

--- crates/art/src/encoders/mod.rs
+++ crates/art/src/encoders/mod.rs
@@ -2,8 +2,10 @@ pub mod command_encoder;
 pub mod compute_pass_encoder;
 pub mod render_pass_encoder;
 pub mod command_buffer;
+pub mod render_bundle_encoder;
 
 pub use command_encoder::*;
 pub use compute_pass_encoder::*;
 pub use render_pass_encoder::*;
 pub use command_buffer::*;
+pub use render_bundle_encoder::*;
--- crates/art/src/pipeline_setup/mod.rs
+++ crates/art/src/pipeline_setup/mod.rs
@@ -1,5 +1,3 @@
 pub mod compute_pipeline;
-pub mod render_bundle_encoder;
 
 pub use compute_pipeline::*;
-pub use render_bundle_encoder::*;
