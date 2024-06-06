ea82c3893d677cd03f4c3e9031490d0e9e01d5ef
Daniel <in.lieu.of.leu@gmail.com>
2024-06-06 10:15:17 +0100

    Reorder kill_non_terminating

--- crates/generator/src/available_calls/kill_non_terminating.rs
+++ crates/generator/src/available_calls/kill_non_terminating.rs
@@ -5,42 +5,52 @@ pub fn kill_non_terminating(available_api_calls: &mut Vec<APICall>) {
         PrintWGSLLanguageFeatures() => false,
         PrintPreferredCanvasFormat() => false,
         CreateArray() => false,
+
         CreateAdapter() => false,
         PrintAdapterInfo(_) => false,
+
         CreateDevice(_) => false,
         PrintDeviceInfo(_) => false,
-        WaitSubmittedWork(_) => false,
+        DestroyDevice(_) => false,
+
         CreateRandomBuffer(_) => false,
-        WriteBuffer(_, _, _) => false,
         PrintBufferInfo(_) => false,
-        DestroyBuffer(_) => false,
         ReadMappedBuffer(_) => false,
+        WriteBuffer(_, _, _) => false,
+        DestroyBuffer(_) => false,
+
+        CreateRandomTexture(_) => false,
+        PrintTextureInfo(_) => false,
+        WriteTexture(_, _, _) => false,
+        CreateTextureView(_) => false,
+        DestroyTexture(_) => false,
+
+        CreateSampler(_) => false,
+
+        CreateShaderModuleCompute(_) => false,
+        CreateShaderModuleRender(_) => false,
+        PrintShaderModuleInfo(_) => false,
+
+        CreateComputeBindGroupLayout(_) => false,
+        CreateComputePipelineLayout(_, _) => false,
+
+        CreateComputePipeline(_, _, _) => false,
+        CreateComputePipelineAsync(_, _, _) => false,
+
+        CreateRenderPipeline(_, _) => false,
+        CreateRenderPipelineAsync(_, _) => false,
+
+        CreateCommandEncoder(_) => false,
         ClearBuffer(_, _) => false,
         CopyBufferToBuffer(_, _, _) => false,
         CopyBufferToTexture(_, _, _) => false,
         CopyTextureToBuffer(_, _, _) => false,
         CopyTextureToTexture(_, _, _) => false,
-        CreateRandomTexture(_) => false,
-        WriteTexture(_, _, _) => false,
-        PrintTextureInfo(_) => false,
-        CreateTextureView(_) => false,
-        DestroyTexture(_) => false,
-        CreateSampler(_) => false,
-        CreateCommandEncoder(_) => false,
         InsertCommandEncoderDebugMarker(_) => false,
         PushCommandEncoderDebugGroup(_) => false,
         PopCommandEncoderDebugGroup(_) => true,
+
         CreateComputePass(_) => false,
-        CreateShaderModuleCompute(_) => false,
-        CreateShaderModuleRender(_) => false,
-        PrintShaderModuleInfo(_) => false,
-        CreateComputeBindGroupLayout(_) => false,
-        CreateComputePipelineLayout(_, _) => false,
-        CreateCommandBuffer(_) => true,
-        CreateComputePipeline(_, _, _) => false,
-        CreateComputePipelineAsync(_, _, _) => false,
-        CreateRenderPipeline(_, _) => false,
-        CreateRenderPipelineAsync(_, _) => false,
         SetComputePassPipeline(_, _) => true,
         SetComputePassBindGroupTemplate(_, _, _) => true,
         SetComputePassWorkgroups(_) => true,
@@ -49,12 +59,16 @@ pub fn kill_non_terminating(available_api_calls: &mut Vec<APICall>) {
         PushComputePassDebugGroup(_) => false,
         PopComputePassDebugGroup(_) => true,
         EndComputePass(_) => true,
+
         CreateRenderPass(_, _) => false,
         SetRenderPassPipeline(_, _) => false,
+
+        CreateCommandBuffer(_) => true,
         SubmitQueueRandom(_, _) => true,
-        // AddUncapturedErrorListener(_) => false,
+        WaitSubmittedWork(_) => false,
+
         PushRandomErrorScope(_) => false,
         PopErrorScope(_) => true,
-        DestroyDevice(_) => false
+        // AddUncapturedErrorListener(_) => false,
     });
 }
