016a4aaa071fe19124a99c01fd626eb4fb1e6a0c
Daniel <in.lieu.of.leu@gmail.com>
2024-06-10 11:33:54 +0100

    Set createRenderPipeline() layout back to 'auto'

--- crates/acv/src/to_javascript/pipeline_setup/render_pipeline.rs
+++ crates/acv/src/to_javascript/pipeline_setup/render_pipeline.rs
@@ -40,31 +40,9 @@ pub fn render_pipeline_to_js(api_call: &APICall, created_resource: &Resource) ->
         primitive: {{
             topology: \"triangle-list\",
         }},
-        layout: {}.createPipelineLayout({{
-            bindGroupLayouts: [
-                {}.createBindGroupLayout({{
-                    entries: [
-                        {{
-                            binding: 0,
-                            visibility: GPUShaderStage.FRAGMENT,
-                            buffer: {{
-                                type: \"uniform\"
-                            }}
-                        }},
-                        {{
-                            binding: 1,
-                            visibility: GPUShaderStage.FRAGMENT,
-                            buffer: {{
-                                type: \"storage\"
-                            }}
-                        }}
-                    ]
-                }})
-            ]
-        }}),
+        layout: \"auto\"
     }});", 
-                    render_pipeline.var_name, device.var_name, shader_module.var_name, shader_module.var_name,
-                    device.var_name, device.var_name)
+                    render_pipeline.var_name, device.var_name, shader_module.var_name, shader_module.var_name)
             } else {
                 panic!("created_resource for CreateRenderPipeline() call is not a render pipeline!")
             }
