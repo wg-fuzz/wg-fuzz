8d3e7a92ce44e2160e7bec758e3fe32c1aa3a7c4
Daniel <in.lieu.of.leu@gmail.com>
2024-06-10 17:53:33 +0100

    Correct await adapter.requestAdapterInfo() to adapter.info according to updated spec

--- crates/acv/src/to_javascript/program_setup/adapter.rs
+++ crates/acv/src/to_javascript/program_setup/adapter.rs
@@ -39,13 +39,13 @@ pub fn adapter_to_js(api_call: &APICall, created_resource: &Resource) -> String 
         console.log(value);
     }}
 
-    // {{
-    //     const adapterInfo = await {}.requestAdapterInfo();
-    //     console.log(adapterInfo.vendor);
-    //     console.log(adapterInfo.architecture);
-    //     console.log(adapterInfo.device);
-    //     console.log(adapterInfo.description);
-    // }}", 
+    {{
+        const adapter = {};
+        console.log(adapter.info.vendor);
+        console.log(adapter.info.architecture);
+        console.log(adapter.info.device);
+        console.log(adapter.info.description);
+    }}", 
                 adapter.var_name, adapter.var_name, adapter.var_name, 
                 adapter.var_name, adapter.var_name, adapter.var_name)
         }
