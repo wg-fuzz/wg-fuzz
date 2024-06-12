61140a40c0cc37123add0a6e83340279dd38c1f6
Daniel <in.lieu.of.leu@gmail.com>
2024-06-12 19:47:48 +0100

    Update running time back to 20s

--- crates/wg-fuzz/src/lib.rs
+++ crates/wg-fuzz/src/lib.rs
@@ -28,7 +28,7 @@ pub fn fuzz() {
 
     let start_time = chrono::offset::Local::now();
     let mut current_time = chrono::offset::Local::now();
-    while current_time < start_time.checked_add_signed(chrono::TimeDelta::seconds(20000)).unwrap() {
+    while current_time < start_time.checked_add_signed(chrono::TimeDelta::seconds(20)).unwrap() {
         fuzz_once().unwrap();
         current_time = chrono::offset::Local::now();
     }
